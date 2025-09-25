2a5186bdc3162bf16369f346795832948f566333
Nisha <nisha02009@gmail.com>
2025-09-25 13:15:03 +0530

    Refactor: Improve evaluation metrics and script robustness

--- src/train_aux.py
+++ src/train_aux.py
@@ -67,8 +67,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--sarcasm_path', type=str, default='data/train-balanced-sarcasm.csv', help='Path to the sarcasm data file.')
     parser.add_argument('--emotion_path', type=str, default='data/go_emotions_dataset.csv', help='Path to the emotion data file.')
-    parser.add_argument('--sarcasm_model_save_path', type=str, default='models/sarcasm_model.joblib')
-    parser.add_argument('--emotion_model_save_path', type=str, default='models/emotion_model.joblib')
+    parser.add_argument('--sarcasm_model_save_path', type=str, default='models/sarcasm_model.joblib', help='Path to save the sarcasm model.')
+    parser.add_argument('--emotion_model_save_path', type=str, default='models/emotion_model.joblib', help='Path to save the emotion model.')
 
     args = parser.parse_args()
     main(args)
